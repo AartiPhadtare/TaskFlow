c954f8e93fa65eab480a2ae6c13e33cc4a88efca
Aarti <aarti@support.unlocktech.in>
2025-03-18 14:29:42 -0500

    list users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
-
+import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from "./tasks/tasks.component";
+import { NgFor } from '@angular/common';
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'TaskFlow';
+users = DUMMY_USERS;
+selectedUserId?: string;
+
+onSelectUser(id:string){
+  this.selectedUserId = id;
+}
+
+get selectedUser(){
+  return this.users.find((user) => user.id === this.selectedUserId);
+}
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, computed, EventEmitter, Input, output, Output } from '@angular/core';
 
 import { DUMMY_USERS } from '../dummy-users';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+// type User = {
+//   id: string;
+//   avatar: string;
+//   name: string;
+// }
+
+interface User {
+  id: string;
+  avatar: string;
+  name: string;
+}
+
 @Component({
   selector: 'app-user',
   imports: [],
@@ -10,5 +21,19 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
+// @Input({required:true}) id!:string;
+// @Input({required:true}) avatar!: string;
+// @Input({required:true}) name!: string;
+@Input({required:true}) user!: User;
+@Output() select = new EventEmitter<string>();
+// select = output<string>();
+
+onSelectUser(){
+  this.select.emit(this.user.id);
+}
+
+imagePath(){ 
+  return 'public/' + this.user.avatar;
+}
+
 }
